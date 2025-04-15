7bd9f33e7e217bae2978af2c126c192f16290f16
Piyush Kumar <57319189+spyder7oo@users.noreply.github.com>
2025-04-15 15:03:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.title("Title Hello World")
+
+st.header("Heading Hello World")
+
+st.subheader("Subheader Hello World")
+
+st.text("Text Hello World")
+
+st.markdown("Markdown")
+st.markdown("# Markdown")
+st.markdown("## Markdown")
+st.markdown("### Markdown")
+st.markdown("#### Markdown")
+st.markdown("##### Markdown")
+st.markdown("###### Markdown")
+
+st.markdown("**Bold** Markdown")
+st.markdown("*Italic* Markdown")
+st.markdown("***Bold and Italic*** Markdown")
+
+st.markdown("- First item")
+st.markdown("- Second item")
+
+st.write("Write Hello World")
+st.write("range(1, 10)")
+st.write(range(1, 10))
